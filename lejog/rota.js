ecac4b2a65c535c61788bd0241b48a88ccc1829c
Colin Greenwood <colingreenwood@scrumconnect.com>
2017-02-18 14:52:07 +0000

    Amended rota to show hq sleeper van and updated costs.

--- lejog/rota.js
+++ lejog/rota.js
@@ -8,10 +8,9 @@ function populateTable() {
     addInitialCells();
     addHeaderCols();
     addGrid();
-    addRidingCells();
-    addVanCells();
-    addCamperCells();
+    setAllCells();
     updateTableInputs();
+    addPlaces("reverse");
 }
 
 function addInitialCells() {
@@ -29,40 +28,40 @@ function addInitialCells() {
         <th>Rider</th>\
         </tr>\
         <tr id="a1">\
-        <td><b>A1</b></td>\
+        <td><b>AHQ</b></td>\
     </tr>\
     <tr id="a2">\
-        <td><b>A2</b></td>\
+        <td><b>AS</b></td>\
         </tr>\
         <tr id="a3">\
-        <td><b>A3</b></td>\
+        <td><b>AV1</b></td>\
         </tr>\
         <tr id="a4">\
-        <td><b>A4</b></td>\
+        <td><b>AV2</b></td>\
         </tr>\
         <tr id="b1">\
-        <td><b>B1</b></td>\
+        <td><b>BHQ</b></td>\
         </tr>\
         <tr id="b2">\
-        <td><b>B2</b></td>\
+        <td><b>BS</b></td>\
         </tr>\
         <tr id="b3">\
-        <td><b>B3</b></td>\
+        <td><b>BV1</b></td>\
         </tr>\
         <tr id="b4">\
-        <td><b>B4</b></td>\
+        <td><b>BV2</b></td>\
         </tr>\
         <tr id="c1">\
-        <td><b>C1</b></td>\
+        <td><b>CHQ</b></td>\
         </tr>\
         <tr id="c2">\
-        <td><b>C2</b></td>\
+        <td><b>CS</b></td>\
         </tr>\
         <tr id="c3">\
-        <td><b>C3</b></td>\
+        <td><b>CV1</b></td>\
         </tr>\
         <tr id="c4">\
-        <td><b>C4</b></td>\
+        <td><b>CV2</b></td>\
         </tr>\
         <tr id="miles">\
         <td><b>Miles</b></td>\
@@ -108,18 +107,22 @@ function addGrid() {
 
 // nth-child goes from 1 upwards
 function setCell(rider, cellnum, celltype, miles) {
-    // $("#a1").find('td:nth-child('+cellnum+')').after("<td class=\"camper\">"+"R"+"</td>");
+    // $("#a1").find('td:nth-child('+cellnum+')').after("<td class=\"sleeper\">"+"R"+"</td>");
     if (celltype == "riding") {
         $("#" + rider + "_" + cellnum).addClass("riding");
         $("#" + rider + "_" + cellnum).html("R");
     }
-    else if (celltype == "camper") {
-        $("#" + rider + "_" + cellnum).addClass("camper");
-        $("#" + rider + "_" + cellnum).html("C");
+    else if (celltype == "hq") {
+        $("#" + rider + "_" + cellnum).addClass("hq");
+        $("#" + rider + "_" + cellnum).html("HQ");
     }
     else if (celltype == "van") {
         $("#" + rider + "_" + cellnum).addClass("van");
         $("#" + rider + "_" + cellnum).html("V");
+    }
+    else if (celltype == "sleeper") {
+        $("#" + rider + "_" + cellnum).addClass("sleeper");
+        $("#" + rider + "_" + cellnum).html("S");
     }
     else if (celltype == "break") {
         $("#" + rider + "_" + cellnum).addClass("break");
@@ -149,16 +152,22 @@ function getNoneBlankCell(rider, cellnum) {
 function setGroupCells(group, cellnum, celltype) {
     setCell(group + "1", cellnum, celltype);
     setCell(group + "2", cellnum, celltype);
-    setCell(group + "3", cellnum, celltype);
-    setCell(group + "4", cellnum, celltype);
+    if(celltype=="hq"){
+        setCell(group + "3", cellnum, "van");
+        setCell(group + "4", cellnum, "van");
+    }
+    else{
+        setCell(group + "3", cellnum, celltype);
+        setCell(group + "4", cellnum, celltype);
+    }
 }
 function setMiles(cellnum, speed) {
     setCell("miles", cellnum, "miles", speed);
     var totalmiles = 0;
     $("#miles").find("td").each(function () {
             var thismiles = $(this).html();
-            if (thismiles.length > 0 && thismiles != " " && thismiles.length < 3) {
-                totalmiles += parseInt(thismiles);
+            if (thismiles.length > 0 && thismiles != " " && thismiles.length < 4) {
+                totalmiles += Math.round(thismiles*10)/10;
                 setCell("totalmiles", cellnum, "totalmiles", totalmiles);
                 if (totalmiles >= 999) {
                     $("#totalmiles_" + cellnum).addClass("finished");
@@ -167,16 +176,13 @@ function setMiles(cellnum, speed) {
         }
     );
 }
-function addRidingCells() {
+function setAllCells(){
+    // Each rider goes ride ... rest ... hq/van
+    // With offset for starting
     var aspeed = $("#aspeed").val() / 2;
     var bspeed = $("#bspeed").val() / 2;
     var cspeed = $("#cspeed").val() / 2;
     var cellnum = 1;
-    setGroupCells("a", cellnum, "riding")
-    setGroupCells("b", cellnum, "riding")
-    setGroupCells("c", cellnum, "riding")
-    setMiles(cellnum, "6");
-    cellnum++;
     for (var i = 0; i < shifts_array.length; i++) {
         var numhours = shifts_array[i];
         if (numhours == 0 || numhours == "B") {
@@ -187,94 +193,47 @@ function addRidingCells() {
             cellnum++;
         }
         else {
+            // a ride b duty c rest
             // riding = true ... shift*1
             for (var j = 1; j <= numhours; j++) {
                 setGroupCells("a", cellnum, "riding");
+                setGroupCells("b", cellnum, "hq");
+                setGroupCells("c", cellnum, "sleeper");
                 setMiles(cellnum, aspeed);
                 cellnum++;
             }
 
             for (var j = 1; j <= numhours; j++) {
+                setGroupCells("a", cellnum, "sleeper");
                 setGroupCells("b", cellnum, "riding");
+                setGroupCells("c", cellnum, "hq");
                 setMiles(cellnum, bspeed);
                 cellnum++;
             }
             for (var j = 1; j <= numhours; j++) {
+                setGroupCells("a", cellnum, "hq");
+                setGroupCells("b", cellnum, "sleeper");
                 setGroupCells("c", cellnum, "riding");
                 setMiles(cellnum, cspeed);
                 cellnum++;
             }
         }
     }
-    setGroupCells("a", cellnum, "riding")
-    setGroupCells("b", cellnum, "riding")
-    setGroupCells("c", cellnum, "riding")
-    setMiles(cellnum, "6");
-}
-function addVanCells() {
-    var van1 = "c1,c2,a1,a2,b1,b2,c3,c4,a3,a4,b3,b4,";
-    var van2 = "c1,c3,a1,a3,b1,b3,c2,c2,a2,a4,b2,b4,";
-    var van3 = "c1,c4,a1,a4,b1,b4,c2,c3,a2,a3,b2,b3,";
-    var vanorder = van1 + van2 + van3 + van1 + van2 + van3 + van1 + van2 + van3 + van1 + van2 + van3;
-    var van_array = vanorder.split(',');
-    var cellnum = 2;
-    var vannum = 0;
-    for (var i = 0; i < shifts_array.length; i++) {
-        var numhours = shifts_array[i];
-        if (numhours != 0 && numhours != "B") {
-            var driver1 = van_array[vannum];
-            var passenger1 = van_array[vannum + 1];
-            for (var j = 1; j <= numhours; j++) {
-                cellnum = getNoneBlankCell(driver1, cellnum);
-                setCell(driver1, cellnum, "van");
-                setCell(passenger1, cellnum, "van");
-                cellnum++;
-            }
-            vannum += 2;
+//    setGroupCells("a", cellnum, "riding")
+//    setGroupCells("b", cellnum, "riding")
+//    setGroupCells("c", cellnum, "riding")
+//    setMiles(cellnum, "6");
 
-            var driver2 = van_array[vannum];
-            var passenger2 = van_array[vannum + 1];
-            for (var j = 1; j <= numhours; j++) {
-                setCell(driver2, cellnum, "van");
-                setCell(passenger2, cellnum, "van");
-                cellnum++;
-            }
-            vannum += 2;
-
-            var driver3 = van_array[vannum];
-            var passenger3 = van_array[vannum + 1];
-            for (var j = 1; j <= numhours; j++) {
-                setCell(driver3, cellnum, "van");
-                setCell(passenger3, cellnum, "van");
-                cellnum++;
-            }
-            vannum += 2;
-        }
-    }
 }
 
-function setEmptyToCamper(rider, cellnum) {
+function setEmptyToSleeper(rider, cellnum) {
     var celltype = $("#" + rider + "_" + cellnum).html();
     var mileage = $("#totalmiles_" + cellnum).html();
     if (mileage == " ") {
         removeCell(rider, cellnum);
     }
     else if (celltype == " ") {
-        setCell(rider, cellnum, "camper")
-    }
-}
-
-function addCamperCells() {
-    // fill anything thats blank with camper !
-    // 48+48+48+24 = 168
-    for (j = 1; j <= 168; j++) {
-        for (k = 1; k <= 4; k++) {
-            setEmptyToCamper("a" + k, j);
-            setEmptyToCamper("b" + k, j);
-            setEmptyToCamper("c" + k, j);
-        }
-        setEmptyToCamper("miles", j);
-        setEmptyToCamper("totalmiles", j);
+        setCell(rider, cellnum, "sleeper")
     }
 }
 
@@ -302,7 +261,10 @@ function addPlaces(direction) {
 }
 
 function updateTown(town, mileage) {
-    var towncolspan = 6;
+    var towncolspan = 4;
+    if(town.length>10){
+        var towncolspan = 6;
+    }
     for (j = 1; j <= maxcells; j++) {
         var total = parseInt($("#totalmiles_" + j).html());
         if (total > mileage) {
